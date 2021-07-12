c21f2121afc003e3ebd83a039adaa0f07aea089b
vaibhav joshi <vaibhavj478@gmail.com>
2021-07-12 07:19:53 -0700

    intail

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+console.log("hello");
+
+ function showNav(){
+ let v=document.getElementById("navBtn");
+  let elem=document.getElementsByClassName("hidDen");
+   console.log(elem[0]);
+
+//  if(elem.style.display === "none"){
+
+//     elem.style.display = "inline-block";
+
+//  }else{
+//     elem.style.display = "none";
+//  }
+}
+
+
+
+
+
